7c36557ea12b9a454ca2a0f628dd8c2de57a7baf
gh-ashkan <ashkanghavami8@gmail.com>
2025-05-20 10:54:14 +0330

    show last courss

--- frontend/src/Components/CourseBox/CourseBox.js
+++ frontend/src/Components/CourseBox/CourseBox.js
@@ -1,91 +1,94 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import CircleSpinner from "../CircleSpinner/CircleSpinner";
 
 import "./CourseBox.css";
 
-export default function CourseBox() {
+export default function CourseBox(props) {
+  const [isImgShow, setIsImgShow] = useState(false);
 
-  const [isImgShow, setIsImgShow] = useState(false)
-
-  const onImageLoaded = () => setIsImgShow(true)
+  const onImageLoaded = () => setIsImgShow(true);
 
   const onImageError = () => {
     // Codes
-  }
+  };
 
   return (
-    <div class="col-4">
-      <div class="course-box">
-        <a href="#">
+    <div className="col-4">
+      <div className="course-box">
+        <Link to={`/course-info/${props.shortName}`}>
           <img
-            src="/images/courses/fareelancer.png"
+            src={props.cover}
             // src="https://placeimg.com/295/295/any/tech?t=190129384"
             alt="Course img"
-            class="course-box__img"
+            className="course-box__img"
             onLoad={onImageLoaded}
             onError={onImageError}
           />
-          {
-            !isImgShow && (
-              <CircleSpinner />
-            )
-          }
-        </a>
-        <div class="course-box__main">
-          <a href="#" class="course-box__title">
-            دوره پروژه محور متخصص جنگو
-          </a>
+          {!isImgShow && <CircleSpinner />}
+        </Link>
+        <div className="course-box__main">
+          <Link to={`/course-info/${props.shortName}`} className="course-box__title">
+            {props.name}
+          </Link>
 
-          <div class="course-box__rating-teacher">
-            <div class="course-box__teacher">
-              <i class="fas fa-chalkboard-teacher course-box__teacher-icon"></i>
-              <a href="#" class="course-box__teacher-link">
+          <div className="course-box__rating-teacher">
+            <div className="course-box__teacher">
+              <i className="fas fa-chalkboard-teacher course-box__teacher-icon"></i>
+              <a href="#" className="course-box__teacher-link">
                 رضا دولتی
               </a>
             </div>
-            <div class="course-box__rating">
+            <div className="course-box__rating">
               <img
                 src="/images/svgs/star.svg"
                 alt="rating"
-                class="course-box__star"
+                className="course-box__star"
               />
               <img
                 src="/images/svgs/star_fill.svg"
                 alt="rating"
-                class="course-box__star"
+                className="course-box__star"
               />
               <img
                 src="/images/svgs/star_fill.svg"
                 alt="rating"
-                class="course-box__star"
+                className="course-box__star"
               />
               <img
                 src="/images/svgs/star_fill.svg"
                 alt="rating"
-                class="course-box__star"
+                className="course-box__star"
               />
               <img
                 src="/images/svgs/star_fill.svg"
                 alt="rating"
-                class="course-box__star"
+                className="course-box__star"
               />
             </div>
           </div>
 
-          <div class="course-box__status">
-            <div class="course-box__users">
-              <i class="fas fa-users course-box__users-icon"></i>
-              <span class="course-box__users-text">500</span>
+          <div className="course-box__status">
+            <div className="course-box__users">
+              <i className="fas fa-users course-box__users-icon"></i>
+              <span className="course-box__users-text">500</span>
             </div>
-            <span class="course-box__price">1,000,000</span>
+            <span className="course-box__price">
+              {
+                props.price === 0 ? 'رایگان' : props.price.toLocaleString()
+              }
+            </span>
           </div>
         </div>
 
-        <div class="course-box__footer">
-          <a href="#" class="course-box__footer-link">
+        <div className="course-box__footer">
+          <Link
+            to={`/course-info/${props.shortName}`}
+            className="course-box__footer-link"
+          >
             مشاهده اطلاعات
-            <i class="fas fa-arrow-left course-box__footer-icon"></i>
-          </a>
+            <i className="fas fa-arrow-left course-box__footer-icon"></i>
+          </Link>
         </div>
       </div>
     </div>
--- frontend/src/Components/LastCourses/LastCourses.js
+++ frontend/src/Components/LastCourses/LastCourses.js
@@ -1,30 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CourseBox from "../CourseBox/CourseBox";
 import SectionHeader from "../SectionHeader/SectionHeader";
 
 import "./LastCourses.css";
 
 export default function LastCourses() {
+  const [courses, setCourses] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:4000/v1/courses`)
+      .then((res) => res.json())
+      .then((allCourses) => setCourses(allCourses));
+  }, []);
+
   return (
     <>
-      <div class="courses">
-        <div class="container">
+      <div className="courses">
+        <div className="container">
           <SectionHeader
             title="جدیدترین دوره ها"
             desc="سکوی پرتاپ شما به سمت موفقیت"
             btnTitle="تمامی دوره ها"
-            btnHref='courses'
+            btnHref="courses"
           />
 
-          <div class="courses-content">
-            <div class="container">
-              <div class="row">
-                <CourseBox />
-                <CourseBox />
-                <CourseBox />
-                <CourseBox />
-                <CourseBox />
-                <CourseBox />
+          <div className="courses-content">
+            <div className="container">
+              <div className="row">
+                {courses.splice(0, 6).map((course) => (
+                  <CourseBox {...course} />
+                ))}
               </div>
             </div>
           </div>
